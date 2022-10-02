09f5ca47ee36fb70bae938d57afe00d405cb429f
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-10-02 17:11:32 +0530

    Add FileContact

---
+++ FileDemoo/FIleOpration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDemoo
+{
+    public class FIleOpration
+    {
+        public void ReadFromStremReader(string path)
+        {
+            using(StreamReader sr =File.OpenText(path))
+            {
+                string s = "";
+                while((s= sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
---
+++ FileDemoo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileDemoo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\91762\bridgelabz\FileDemoo\FileDemoo\Demo.txt";
+          FIleOpration fIleOpration = new FIleOpration();
+            fIleOpration.ReadFromStremReader(path);
+        //}
+    }
+}
